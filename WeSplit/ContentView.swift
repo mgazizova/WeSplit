81ff8689d8dbc72c8ee2833ab258a420dfe3953e
Mariia Gazizova <mgazizova5@gmail.com>
2024-05-19 15:37:37 +0200

    Form, Navigation stack, ForEach, @State

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Hermione", "Ron", "Harry", "Sedric"] // not a @State because it is a constant
+    @State private var selectedStudent = ""
+    @State private var tapCount = 0
+    @State private var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Button("Tap count: \(tapCount)") {
+                tapCount += 1 // doesn't need a $ because it is changed manualy
+            }
+            TextField("Enter your name", text: $name) // need a $ because it is changed by user, so there sould be an ability to write the updates into @State -- Two-ways binding
+            Text("Your name is \(name)") // One-way binding -- only read the value
+            
+            Picker("Select your student", selection: $selectedStudent) { // Two-ways binding selectedStudent -- can be changed
+                ForEach(students, id: \.self) { // id: \.self - to identify views uniquely, strings in students should be unique otherwise there is problems with defining exact element and picking it
+                    Text($0)
+                }
+            }
+            
+            ForEach(0..<10) { number in
+                Text("Row \(number)")
+            }
         }
-        .padding()
+    }
+}
+
+struct SafeAreaCheck: View {
+    var body: some View {
+        NavigationStack { // adds a title + navigation + content inside safe area
+            Form {
+                Text("Hello world!")
+            }
+            .navigationTitle("Title")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
 #Preview {
     ContentView()
+    //    SafeAreaCheck()
 }
--- WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplitApp.swift
@@ -12,6 +12,7 @@ struct WeSplitApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+//            SafeAreaCheck()
         }
     }
 }
